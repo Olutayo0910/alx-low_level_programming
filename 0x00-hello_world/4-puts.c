a84e658ee761614ce0fde721764362a1743d0d2d
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-02-16 12:56:40 -0800

    until done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
-/* use function puts
+/**
+ * use function puts
  * You are not allowed to use printf
  *
- * Return: Always 0 (success
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle")
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
