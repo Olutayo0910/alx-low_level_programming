09c164aaacbc3dc486681bcb2bcf5c5479cc1a30
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-02-16 12:25:56 -0800

    c language

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
-/* use function puts. Return 0 */
-
+/* use function puts
+ * You are not allowed to use printf
+ *
+ * Return: Always 0 (success
+ */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle\n")
