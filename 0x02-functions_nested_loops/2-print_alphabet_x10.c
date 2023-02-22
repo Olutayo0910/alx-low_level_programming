588c48f32532376e1785121def5020a8db703a56
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-02-22 07:56:02 -0800

    print x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	char la;
+	int i;
+
+
+	
+	for (i = 0; i < 10; i++)
+	
+	{
+		la = 'a'
+		while (la <= 'z')
+		{
+			_putchar(la);
+			la++;
+		}
+		_putchar("\n");
+	}
+}
