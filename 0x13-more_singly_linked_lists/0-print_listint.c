686c73685ac2181d3bcdf18520bf8e51e3db061d
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-04-04 15:01:29 -0700

    print all element in list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,14 +10,12 @@
  */
 size_t print_listint(const listint_t *h)
 {
-    size_t count = 0;
+	size_t i;
 
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        count++;
-        h = h->next;
-    }
-
-    return (count);
+	for (i = 0; h; i++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (i);
 }
