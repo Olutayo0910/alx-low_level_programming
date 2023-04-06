27271365215cb36814f1fc263eeae6f2abd6c2c4
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-04-05 21:48:01 -0700

    print list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,11 +1,26 @@
 #include "lists.h"
 /**
-  * print_listint_safe - define function
-  * @head:
-  * Return: what does it return?
-  */
+* print_listint_safe - prints the circular linked list
+* @head: head
+* Return: return
+*/
 size_t print_listint_safe(const listint_t *head)
 {
-	head++;
-	return (0);
+size_t a;
+listint_t *b = (listint_t *)head;
+
+a = 0;
+while (b && b > b->next)
+{
+printf("[%p] %d\n", (void *)b, b->n);
+b = b->next;
+a++;
+}
+if (b)
+{
+printf("[%p] %d\n", (void *)b, b->n);
+printf("-> [%p] %d\n", (void *)b->next, b->next->n);
+a++;
+}
+return (a);
 }
