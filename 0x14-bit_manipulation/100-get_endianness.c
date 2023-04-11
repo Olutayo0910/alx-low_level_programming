c12ce71a779d466343d806bb45c8a3d2866857fc
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-04-10 22:11:20 -0700

    corrected

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -7,15 +7,11 @@
  */
 int get_endianness(void)
 {
-    unsigned int x = 1;
-    char *c = (char *)&x;
+	unsigned int x;
+	char *c;
 
-    if (*c == 1) 
-    {
-        return (1);
-    } 
-    else 
-    {
-        return (0);
-    }
-}	
+	x = 1;
+	c = (char *) &x;
+
+	return ((int)*c);
+}
