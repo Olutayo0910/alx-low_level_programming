3520dfdece5264f55a1090b37437096bc4889f33
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-04-14 21:40:55 -0700

    copies content to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,39 +1,38 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
-
-#define BUFSIZE 1024
+#include "main.h"
 
 /**
- * main - copies the content of a file to another file
- * @argc: number of arguments passed
- * @argv: array of arguments passed
+ * main - copies the content of a file to another file.
  *
- * Return: 0 on success, or exit with error codes
+ * @ac: number of arguments.
+ * @av: array of arguments.
+ *
+ * Return: 0 on success, or an exit status on failure.
  */
-int main(int argc, char **argv)
+int main(int ac, char **av)
 {
-	int fd_from, fd_to, len;
-	char buf[BUFSIZE];
+	int fd_from, fd_to, rd, wr;
+	char buffer[1024];
 
-	if (argc != 3)
+	if (ac != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 
-	fd_from = open(argv[1], O_RDONLY);
+	fd_from = open(av[1], O_RDONLY);
 	if (fd_from == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 
-	while ((len = read(fd_from, buf, BUFSIZE)) > 0)
-		if (write(fd_to, buf, len) != len)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	while ((rd = read(fd_from, buffer, 1024)) > 0)
+	{
+		wr = write(fd_to, buffer, rd);
+		if (wr == -1)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+	}
 
-	if (len == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	if (rd == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 
 	if (close(fd_from) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
