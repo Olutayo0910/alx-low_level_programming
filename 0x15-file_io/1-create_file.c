c6da0785746fd6b2c9d08782420ae874a911a5af
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-04-14 21:27:09 -0700

    updated

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,27 +11,27 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int fd, written = 0;
+        int fd, written = 0;
 
-    if (!filename)
-        return (-1);
+        if (!filename)
+                return (-1);
 
-    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (fd == -1)
-        return (-1);
+        fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+        if (fd == -1)
+                return (-1);
 
-    if (text_content)
-    {
-        while (text_content[written])
-            written++;
-        if (write(fd, text_content, written) != written)
+        if (text_content)
         {
-            close(fd);
-            return (-1);
+                while (text_content[written])
+                        written++;
+                if (write(fd, text_content, written) != written)
+                {
+                        close(fd);
+                        return (-1);
+                }
         }
-    }
 
-    close(fd);
+        close(fd);
 
-    return (1);
+        return (1);
 }
