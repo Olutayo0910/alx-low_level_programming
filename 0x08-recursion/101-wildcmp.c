462430695d02a3f574b9deb9c8d3eb361b22592b
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-06-07 05:44:36 -0700

    wildcmp

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * wildcmp - Compares two strings considering wildcard character *
+ * @s1: The first string to compare
+ * @s2: The second string to compare
+ *
+ * Return: 1 if strings can be considered identical, 0 otherwise
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '\0')
+			return (1);
+		if (*s1 != '\0' && wildcmp(s1 + 1, s2))
+			return (1);
+		if (wildcmp(s1, s2 + 1))
+			return (1);
+	}
+
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	return (0);
+}
