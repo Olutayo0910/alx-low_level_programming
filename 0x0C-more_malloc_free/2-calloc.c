5010a131db40135a3025b48ff4aae205dabd5ddf
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-06-16 08:27:28 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,15 +16,15 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
-		return NULL;
+		return (NULL);
 
 	ptr = malloc(nmemb * size);
 
 	if (ptr == NULL)
-		return NULL;
+		return (NULL);
 
 	for (i = 0; i < (nmemb * size); i++)
 		ptr[i] = 0;
 
-	return ptr;
+	return (ptr);
 }
