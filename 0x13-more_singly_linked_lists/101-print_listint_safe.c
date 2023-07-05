dee1e33006184a6ec9381a1ebcca621ee1dc5935
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-07-05 15:26:34 -0700

    difference

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - Prints a listint_t linked list.
+ * @head: Pointer to the head of the list.
+ *
+ * Return: The number of nodes in the list.
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t count = 0;
+	long int difference;
+
+	while (head)
+	{
+		difference = head - head->next;
+		count++;
+		printf("[%p] %d\n", (void *)head, head->n);
+		if (difference > 0)
+			head = head->next;
+		else
+		{
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			break;
+		}
+	}
+
+	return (count);
+}
