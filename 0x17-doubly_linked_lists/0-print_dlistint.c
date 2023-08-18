63ed331baf53946358500ae3bfa4f9aa0c9ed929
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-08-18 08:27:05 -0700

    po

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,4 +1,4 @@
-#include <lists.h>
+#include "lists.h"
 
 /**
  * print_dlistint - Print all the elements of a doubly linked list
@@ -8,14 +8,24 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t count = 0;
+        size_t count = 0;
 
-	while (h != NULL)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-		count++;
-	}
+        if (h == NULL)
+        {
+            return (count);
+        }
 
-	return (count);
+        while (h->prev != NULL)
+        {
+            h = h->prev;
+        }
+
+        while (h != NULL)
+        {
+                printf("%d\n", h->n);
+                h = h->next;
+                count++;
+        }
+
+        return (count);
 }
